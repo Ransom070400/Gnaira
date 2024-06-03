9a30af8cfd1a955856058098d084edc09b37be9c
Eze Ransom <148370144+Ransom070400@users.noreply.github.com>
2024-06-03 22:50:32 +0100

    Update Gnaira.sol

--- contracts/Gnaira.sol
+++ contracts/Gnaira.sol
@@ -1,19 +1,95 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Gnaira {
+    string public name;
+    string public symbol;
+    uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
 
-contract GNaira is ERC20 {
-  constructor() ERC20("GNaira", "GN") {}
+    uint256 public _totalSupply;
 
-  function mint(address to, uint256 amount) public {
-    _mint(to, amount);
-  }
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
 
-  function burn(uint256 amount) public {
-    _burn(msg.sender, amount);
-  }
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    constructor() public {
+        name = "GNaira";
+        symbol = "GN";
+        decimals = 18;
+        _totalSupply = 100000000000000000000000000;
 
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
 
-  
+    function totalSupply() public view returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    function mint(address to, uint tokens) public {
+        _totalSupply = safeAdd(_totalSupply, tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(address(0), to, tokens);
+    }
+
+    function burn(uint tokens) public {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        _totalSupply = safeSub(_totalSupply, tokens);
+        emit Transfer(msg.sender, address(0), tokens);
+    }
+
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
+        require(b <= a); c = a - b;
+    }
+
+    function safeMul(uint a, uint b) internal pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint a, uint b) internal pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
