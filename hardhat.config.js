8e1d4b5e1e01674d2d47bbcc91fa9f0edaa56c9d
Eze Ransom <148370144+Ransom070400@users.noreply.github.com>
2024-06-03 22:55:03 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.5.0",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
